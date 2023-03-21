e3d98f36536a2fe84b7a6aaffdc9f9d3eb6cf213
m4rcelnoel <marcel.gruber@buddster.de>
2023-03-21 01:40:26 +0100

    problem with comment
    removed comment

--- App.js
+++ App.js
@@ -16,16 +16,16 @@ const Main = () => {
     <PaperProvider>
       <StatusBar style="light"/>
       <NavigationContainer>
-        <Tab.Navigator screenOptions={{ headerShown: false, tabBarStyle: { backgroundColor: '#0C0F16'}}}> {/* Verwende den Tab.Navigator mit einigen Optionen */}
+        <Tab.Navigator screenOptions={{ headerShown: false, tabBarStyle: { backgroundColor: '#0C0F16'}}}>
           <Tab.Screen name="Home" component={Home} options={{
             tabBarLabel: 'Home', // Legt den Textbeschriftung des Tabs auf "Home" fest
             tabBarIcon: ({ color, size }) => ( // Definiert das Symbol für den Tab
-              <Ionicons name="home" color={color} size={size} /> // Das Symbol ist ein Home-Icon von Ionicons mit der Farbe und Größe, die der Navigator übergeben hat
+              <Ionicons name="home" color={color} size={size} /> /* Das Symbol ist ein Home-Icon von Ionicons mit der Farbe und Größe, die der Navigator übergeben hat */
             ),}} />
           <Tab.Screen name="Info" component={ContactScreen} options={{
             tabBarLabel: 'Info', // Legt die Textbeschriftung des Tabs auf "Info" fest
             tabBarIcon: ({ color, size }) => ( // Definiert das Symbol für den Tab
-              <Ionicons name="information-outline" color={color} size={size} /> // Das Symbol ist ein Informations-Icon von Ionicons mit der Farbe und Größe, die der Navigator übergeben hat
+              <Ionicons name="information-outline" color={color} size={size} /> /* Das Symbol ist ein Informations-Icon von Ionicons mit der Farbe und Größe, die der Navigator übergeben hat */
             ),}} />
         </Tab.Navigator>
       </NavigationContainer>
